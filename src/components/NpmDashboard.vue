2adecda57d000f3c3aa88afb017e5e02b3f09a50
Julien Calixte <julienc@bam.tech>
2020-05-21 20:08:56 +0200

    :art: (stars) put star count in table

--- src/components/NpmDashboard.vue
+++ src/components/NpmDashboard.vue
@@ -1,20 +1,40 @@
 <template>
   <div class="npm-dashboard">
-    <h2>{{ name }} ({{ result.stars }} étoiles)</h2>
+    <h2>{{ name }}</h2>
     <table>
       <tr>
         <th>mois</th>
         <th>semaine</th>
+        <th>
+          <svg
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            stroke="currentColor"
+            stroke-width="2"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="css-i6dzq1"
+          >
+            <polygon
+              points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
+            ></polygon>
+          </svg>
+        </th>
       </tr>
       <tr>
         <td>{{ result.totalMonth }}</td>
         <td>{{ result.totalWeek }}</td>
+        <td>{{ result.stars }}</td>
       </tr>
     </table>
 
-    <svg viewBox="0 0 100 100">
-      <polyline :points="graphPoints" />
-    </svg>
+    <div class="graph">
+      <svg viewBox="0 0 100 100">
+        <polyline :points="graphPoints" />
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -52,15 +72,17 @@ export default defineComponent({
     }
   }
 
-  svg {
-    width: 100px;
-    height: 100px;
+  .graph {
+    svg {
+      width: 100px;
+      height: 100px;
 
-    polyline {
-      fill: none;
-      stroke: green;
-      stroke-width: 2;
-      text-align: center;
+      polyline {
+        fill: none;
+        stroke: green;
+        stroke-width: 2;
+        text-align: center;
+      }
     }
   }
 }
