dfc2b5ac25de217f43a57feb00a4609d5dd1c0fb
Julien Calixte <julienc@bam.tech>
2020-05-22 12:41:20 +0200

    :art: (app) change style

--- src/components/NpmDashboard.vue
+++ src/components/NpmDashboard.vue
@@ -10,12 +10,10 @@
             viewBox="0 0 24 24"
             width="24"
             height="24"
-            stroke="currentColor"
             stroke-width="2"
             fill="none"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="css-i6dzq1"
           >
             <polygon
               points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
@@ -101,7 +99,7 @@ export default defineComponent({
 
       polyline {
         fill: none;
-        stroke: green;
+        stroke: #43cfcb;
         stroke-width: 2;
         text-align: center;
       }
